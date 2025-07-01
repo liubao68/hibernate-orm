c4d766c4c4718fac59272861317fbea8a337cdd7
liubao68 <bismy@qq.com>
2025-07-01 16:23:42 +0800

    HHH-19365 GaussDB Dialect Support-rebase

--- hibernate-core/src/test/java/org/hibernate/orm/test/filter/FilterParameterTests.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/filter/FilterParameterTests.java
@@ -22,7 +22,7 @@ import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.community.dialect.AltibaseDialect;
 import org.hibernate.community.dialect.FirebirdDialect;
 import org.hibernate.community.dialect.InformixDialect;
-import org.hibernate.dialect.GaussDBDialect;
+import org.hibernate.community.dialect.GaussDBDialect;
 import org.hibernate.dialect.HANADialect;
 import org.hibernate.dialect.CockroachDialect;
 import org.hibernate.dialect.DB2Dialect;
--- hibernate-core/src/test/java/org/hibernate/orm/test/jpa/lock/LockTest.java
+++ hibernate-core/src/test/java/org/hibernate/orm/test/jpa/lock/LockTest.java
@@ -21,7 +21,7 @@ import org.hibernate.TransactionException;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.community.dialect.AltibaseDialect;
 import org.hibernate.community.dialect.FirebirdDialect;
-import org.hibernate.dialect.GaussDBDialect;
+import org.hibernate.community.dialect.GaussDBDialect;
 import org.hibernate.dialect.HANADialect;
 import org.hibernate.dialect.CockroachDialect;
 import org.hibernate.community.dialect.DerbyDialect;
